862113151a4200a584324cfb930056ae02174852
Alejandro Jimenez Garcia <alejandj@c2r9s3.42madrid.com>
2025-01-29 15:05:48 +0100

    bn

--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: alejandj <alejandj@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/23 15:47:33 by alejandj          #+#    #+#             */
-/*   Updated: 2025/01/23 16:10:54 by alejandj         ###   ########.fr       */
+/*   Updated: 2025/01/29 13:41:15 by alejandj         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,8 @@
 
 #include <unistd.h>
 #include <stdlib.h>
+#include <stdio.h>
+#include <fcntl.h>
 
 char    *get_next_line(int fd);
 
