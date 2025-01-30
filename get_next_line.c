2407077a9dcf6fb5df2199e6757ee61458f0e524
Alejandro Jimenez Garcia <alejandj@c2r8s1.42madrid.com>
2025-01-30 16:31:37 +0100

    En proceso...

---
+++ get_next_line.c
@@ -0,0 +1,53 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: alejandj <alejandj@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/30 15:23:31 by alejandj          #+#    #+#             */
+/*   Updated: 2025/01/30 16:28:40 by alejandj         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+char	*get_next_line(int fd)
+{
+	char		buffer[BUFFER_SIZE];
+	ssize_t		bytes_read;
+	static char	*line;
+	int			i;
+	char		temp[100];
+
+	if (fd == -1)
+		return (NULL);
+	bytes_read = read(fd, buffer, BUFFER_SIZE);
+	if (bytes_read <= 0)
+		return (NULL);
+	i = 0;
+	while (buffer[i] != '\n' || buffer[i] != '\0')
+	{
+		temp[i] = buffer[i];
+		i++;
+	}
+	temp[i] = '\0';
+	ft_strlcat(line, temp, ft_strlen(temp) + i);
+	line = ft_strjoin(line, temp);
+	bytes_read = read(fd, buffer, BUFFER_SIZE);
+	return (line);
+}
+
+int	main(void)
+{
+	int		fd;
+	char	*line;
+
+	fd = open("test.txt", O_RDONLY);
+	while ((line = get_next_line(fd)))
+	{
+		printf("%s", line);
+		free(line);
+	}
+	close(fd);
+}
