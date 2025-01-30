20a1110c46f5352ea6e8f119d716d209c8fabd2c
Alejandro Jimenez Garcia <alejandj@c2r8s1.42madrid.com>
2025-01-30 15:22:15 +0100

    prueba

--- get_next_line.h
+++ get_next_line.h
@@ -6,18 +6,22 @@
 /*   By: alejandj <alejandj@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/23 15:47:33 by alejandj          #+#    #+#             */
-/*   Updated: 2025/01/29 13:41:15 by alejandj         ###   ########.fr       */
+/*   Updated: 2025/01/30 12:29:08 by alejandj         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE
-#define GET_NEXT_LINE
+# define GET_NEXT_LINE
 
-#include <unistd.h>
-#include <stdlib.h>
-#include <stdio.h>
-#include <fcntl.h>
+# include <fcntl.h>
+# include <stddef.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
 
-char    *get_next_line(int fd);
+char	*get_next_line(int fd);
+size_t	ft_strlen(const char *s);
+size_t	ft_strlcat(char *dst, const char *src, size_t size);
+char	*ft_strjoin(char const *s1, char const *s2);
 
 #endif
--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -6,8 +6,66 @@
 /*   By: alejandj <alejandj@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/29 12:59:09 by alejandj          #+#    #+#             */
-/*   Updated: 2025/01/29 12:59:18 by alejandj         ###   ########.fr       */
+/*   Updated: 2025/01/30 12:28:38 by alejandj         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
+
+size_t	ft_strlen(const char *s)
+{
+	int	i;
+
+	i = 0;
+	while (s[i])
+		i++;
+	return (i);
+}
+
+size_t	ft_strlcat(char *dst, const char *src, size_t size)
+{
+	size_t	i;
+	size_t	origin_size_dst;
+	size_t	origin_size_src;
+
+	origin_size_dst = ft_strlen(dst);
+	origin_size_src = ft_strlen(src);
+	if (size <= origin_size_dst)
+		return (size + origin_size_src);
+	i = 0;
+	while (src[i] != '\0' && i < size - origin_size_dst - 1)
+	{
+		dst[origin_size_dst + i] = src[i];
+		i++;
+	}
+	dst[origin_size_dst + i] = '\0';
+	return (origin_size_dst + origin_size_src);
+}
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	size_t	i;
+	size_t	j;
+	char	*join;
+
+	if (!s1 || !s2)
+		return (NULL);
+	join = (char *)malloc((ft_strlen(s1) + ft_strlen(s2) + 1) * sizeof(char));
+	if (!join)
+		return (NULL);
+	i = 0;
+	while (s1[i] != '\0')
+	{
+		join[i] = s1[i];
+		i++;
+	}
+	j = 0;
+	while (s2[j] != '\0')
+	{
+		join[i] = s2[j];
+		i++;
+		j++;
+	}
+	join[i] = '\0';
+	return (join);
+}
