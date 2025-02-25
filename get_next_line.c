4faccf83f3db7477dbac6f62e877a46b3846fcba
AlejandroJimenez16 <alejanjiga@gmail.com>
2025-02-25 01:19:08 +0100

    No leaks, pero aun no

--- get_next_line.c
+++ get_next_line.c
@@ -12,34 +12,68 @@
 
 #include "get_next_line.h"
 
-void	ft_strcpy(char *dst, const char *src)
+void	*ft_memset(void *s, int c, size_t n)
 {
-	size_t	i;
+	size_t			i;
+	unsigned char	*p;
 
 	i = 0;
-	while (src[i] != '\0')
+	p = (unsigned char *)s;
+	while (i < n)
 	{
-		dst[i] = src[i];
+		p[i] = (unsigned char)c;
 		i++;
 	}
-	dst[i] = '\0';
+	return (s);
+}
+
+void add_char(char *s, char c)
+{
+    int i;
+
+	i = ft_strlen(s);
+    s[i] = c;
 }
 
 char	*get_next_line(int fd)
 {
-	static char	buffer[BUFFER_SIZE + 1];
+	static char	buffer[BUFFER_SIZE];
 	ssize_t		bytes_read;
 	char		*line;
-	char		*newline;
+	char		*temp;
 	
 	if (fd < 0 || BUFFER_SIZE <= 0)
 		return (NULL);
 	line = ft_strdup("");
 	while (1)
 	{
-		
+		bytes_read = read(fd, buffer, sizeof(buffer));
+		if (bytes_read == 0)
+			break;
+		buffer[bytes_read] = '\0';
+		if (ft_strchr(buffer, '\n'))
+		{
+			int i = 0;
+			while (buffer[i] != '\0')
+			{
+				if (buffer[i] == '\n')
+				{
+					add_char(line, '\n');
+					return (line);
+				}
+				add_char(line, buffer[i]);
+				i++;
+			}
+		}
+		temp = ft_strjoin(line, buffer);
+		free(line);
+		line = temp;
+		ft_memset(buffer, '\0', sizeof(buffer));
 	}
-	
+	if (*line != '\0')
+		return (line);
+	free(line);
+	return (NULL);
 }
 
 int	main(void)
@@ -47,7 +81,7 @@ int	main(void)
 	int		fd;
 	char	*line;
 
-	fd = open("test.txt", O_RDONLY);
+	fd = open("quijote.txt", O_RDONLY);
 	while ((line = get_next_line(fd)))
 	{
 		printf("%s\n", line);
--- get_next_line.h
+++ get_next_line.h
@@ -25,5 +25,6 @@ char	*ft_strjoin(char const *s1, char const *s2);
 char	*ft_strchr(const char *s, int c);
 char	*ft_strdup(const char *s);
 void	*ft_memcpy(void *dest, const void *src, size_t n);
+void	*ft_memset(void *s, int c, size_t n);
 
 #endif
